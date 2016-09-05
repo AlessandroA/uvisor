bd0ffa3392eb6ac6e2a93b3c65aca9de28fd87c5
Alessandro Angelino <alessandro.angelino@arm.com>
2016-09-05 15:32:12 +0100

    WIP: Add tmp implementation of some vMPU APIs

--- core/system/src/mpu/vmpu_armv8m.c
+++ core/system/src/mpu/vmpu_armv8m.c
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 #include <uvisor.h>
+#include "context.h"
 
 uint32_t vmpu_fault_find_acl(uint32_t fault_addr, uint32_t size)
 {
@@ -36,6 +37,18 @@ void vmpu_load_box(uint8_t box_id)
 
 void vmpu_acl_stack(uint8_t box_id, uint32_t bss_size, uint32_t stack_size)
 {
+    /* The public box is handled separately. */
+    if (box_id == 0) {
+        /* Non-important sanity checks */
+        assert(stack_size == 0);
+
+        /* For backwards-compatibility, the public box uses the legacy stack and
+         * heap. */
+        DPRINTF("[box 0] BSS size: %i. Stack size: %i\r\n", bss_size, stack_size);
+        g_context_current_states[0].sp = __get_PSP();
+        g_context_current_states[0].bss = (uint32_t) __uvisor_config.heap_start;
+        return;
+    }
 }
 
 void vmpu_arch_init(void)
