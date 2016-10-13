04c862defa36ee5abe28a3b9d3aeb49b7d7021b0
Alessandro Angelino <alessandro.angelino@arm.com>
2016-10-13 10:22:32 +0100

    v8M: Add basic fault reporting

--- core/system/src/mpu/vmpu_armv8m.c
+++ core/system/src/mpu/vmpu_armv8m.c
@@ -16,6 +16,15 @@
  */
 #include <uvisor.h>
 #include "context.h"
+#include "halt.h"
+#include "unvic.h"
+
+/* This key needs to be written to AIRCR at every write access. */
+#define SCB_AIRCR_VECTKEY 0x5FAUL
+
+static void vmpu_arch_init_sau(void)
+{
+}
 
 uint32_t vmpu_fault_find_acl(uint32_t fault_addr, uint32_t size)
 {
@@ -24,7 +33,31 @@ uint32_t vmpu_fault_find_acl(uint32_t fault_addr, uint32_t size)
 
 void vmpu_sys_mux_handler(uint32_t lr, uint32_t msp)
 {
-    while(1);
+    /* Get the exception number. */
+    int irqn = ((IPSR_Type) __get_IPSR()).b.ISR - NVIC_OFFSET;
+    if (irqn >= NVIC_OFFSET) {
+        HALT_ERROR(NOT_ALLOWED, "An user IRQ (%d) is being served by the system ISR.\r\n", irqn);
+    }
+
+    /* Beuhave differently depending on the fault. */
+    switch (irqn) {
+    case HardFault_IRQn:
+        HALT_ERROR(FAULT_HARD, "Hard fault.\r\n");
+    case MemoryManagement_IRQn:
+        HALT_ERROR(FAULT_MEMMANAGE, "MemManage fault.\r\n");
+    case BusFault_IRQn:
+        DPRINTF("CFSR: 0x%08X\r\n", SCB->CFSR);
+        DPRINTF("BFAR: 0x%08X\r\n", SCB->BFAR);
+        HALT_ERROR(FAULT_BUS, "Bus fault.\r\n");
+    case UsageFault_IRQn:
+        HALT_ERROR(FAULT_USAGE, "Usage fault.\r\n");
+    case SecureFault_IRQn:
+        DPRINTF("SFSR: 0x%08X\r\n", SAU->SFSR);
+        DPRINTF("SFAR: 0x%08X\r\n", SAU->SFAR);
+        HALT_ERROR(FAULT_SECURE, "Secure fault.\r\n");
+    default:
+        HALT_ERROR(NOT_ALLOWED, "Unrecognized fault: %d.\r\n", irqn);
+    }
 }
 
 void vmpu_switch(uint8_t src_box, uint8_t dst_box)
@@ -53,6 +86,34 @@ void vmpu_acl_stack(uint8_t box_id, uint32_t bss_size, uint32_t stack_size)
 
 void vmpu_arch_init(void)
 {
+    /* AIRCR configurations:
+     *      - Non-secure exceptions are de-prioritized.
+     *      - BusFault, HardFault, and NMI are Secure.
+     *      - SYSRESETREQ functionality is only available to Secure state.
+     */
+    /* TODO: Setup a sensible priority grouping. */
+    SCB->AIRCR = (SCB_AIRCR_VECTKEY << SCB_AIRCR_VECTKEY_Pos) |
+                 (SCB->AIRCR & SCB_AIRCR_ENDIANESS_Msk) | /* Keep unchanged */
+                 (SCB_AIRCR_PRIS_Msk) |
+                 (0 << SCB_AIRCR_BFHFNMINS_Pos) |
+                 (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) | /* Keep unchanged */
+                 (SCB_AIRCR_SYSRESETREQ_Msk) |
+                 (0 << SCB_AIRCR_SYSRESETREQ_Pos) |
+                 (0 << SCB_AIRCR_VECTCLRACTIVE_Pos);
+
+    /* SHCSR configurations:
+     *      - SecureFault exception enabled.
+     *      - UsageFault exception enabled for the selected Security state.
+     *      - BusFault exception enabled.
+     *      - MemManage exception enabled for the selected Security state.
+     */
+    SCB->SHCSR |= (SCB_SHCSR_SECUREFAULTENA_Msk) |
+                  (SCB_SHCSR_USGFAULTENA_Msk) |
+                  (SCB_SHCSR_BUSFAULTENA_Msk) |
+                  (SCB_SHCSR_MEMFAULTENA_Msk);
+
+    /* Setup the SAU. */
+    vmpu_arch_init_sau();
 }
 
 void vmpu_arch_init_hw(void)
